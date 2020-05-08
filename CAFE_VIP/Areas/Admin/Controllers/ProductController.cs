ee040a2b6e0512b6e5dba64a8a55a8553079b209
dolemta9899 <dolemta090899@gmail.com>
2020-05-08 21:01:59 +0700

    nguyên liệu + search nhân viên

--- CAFE_VIP/Areas/Admin/Controllers/ProductController.cs
+++ CAFE_VIP/Areas/Admin/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Models.EF;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,12 @@ namespace CAFE_VIP.Areas.Admin.Controllers
 {
     public class ProductController : Controller
     {
+        CafeDbContext db = new CafeDbContext();
         // GET: Admin/Product
         public ActionResult Index()
         {
-            return View();
+            var lstsp = db.PRODUCTs.OrderBy(x => x.ProductID ).ToList();
+            return View(lstsp);
         }
         public ActionResult Create()
         {
